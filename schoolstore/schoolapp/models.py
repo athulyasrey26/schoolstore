d7efe83c3827eb8d6a5b92f046738ec373ce426f
athulyasrey26 <athulyasrey26@gmail.com>
2023-03-09 09:34:17 +0530

    added

---
+++ schoolstore/schoolapp/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.urls import reverse
+# Create your models here.
+
+
+
+class Department(models.Model):
+    name=models.CharField(max_length=250)
+
+
+    def __str__(self):
+        return self.name
+
+
